93246ac05972b491525537fad57cc40d160db224
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-05-02 10:13:56 -0300

    build: Setup tailwindcss, typescript and husky

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Wesley News</title>
+        <meta
+          name="description"
+          content="Todas as notícias do mundo estarão presentes aqui."
+        />
+      </Head>
+
+      <header>
+        <h1>header</h1>
+      </header>
+
+      <main>
+        <h1>main</h1>
+      </main>
+
+      <footer>
+        <h1>footer</h1>
+      </footer>
+    </div>
+  );
+}
