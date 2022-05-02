08e56cdaf0b7943e0efdf92f637fa5554db06c8c
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-05-02 12:31:35 -0300

    feat: add fonts, favicon and header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,26 @@
 import { ReactElement } from "react";
 
-type LayoutProps = { children: ReactElement };
+type LayoutProps = { children: ReactElement; title: string };
 
-export function Layout({ children }: LayoutProps) {
+export function Layout({ children, title }: LayoutProps) {
   return (
-    <>
-      <header>Wesley News</header>
+    <div className="h-full min-h-screen relative">
+      <header className="w-full h-72 bg-black rounded-b-[6.25rem] pt-11 flex justify-center items-center text-center md:px-32">
+        <img
+          className="w-44 h-20 absolute left-[10%] top-10 cursor-pointer"
+          width={256}
+          height={102}
+          src="/Logo.png"
+          alt="Logomarca do site com o texto: 'Wesley News'"
+        />
+        <h1 className="w-8/12 text-white text-4xl">{title}</h1>
+      </header>
 
       {children}
 
       <footer>
         <small>&copy; Copyright 2022, Wesley News</small>
       </footer>
-    </>
+    </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,23 @@ import { Layout } from "../components/Layout";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Wesley News | Home</title>
         <meta
           name="description"
           content="Todas as notícias do mundo estarão presentes aqui."
         />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
       </Head>
 
       <main>
         <h1>main</h1>
       </main>
-    </div>
+    </>
   );
 }
 
 Home.getLayout = (page) => {
-  return <Layout>{page}</Layout>;
+  return <Layout title="News">{page}</Layout>;
 };
