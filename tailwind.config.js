4168f1fe9a36d1f76ed8ada4ef9a77eba2e7faf3
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-05-02 10:44:25 -0300

    fix: missing rules to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ["./src/**/*.{js, jsx, ts, tsx}"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
