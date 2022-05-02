c1e04a8ff3519219960cec32a43d1d0ad83eeabf
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-05-02 12:38:35 -0300

    fix: chage logo png to svg

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,12 +5,12 @@ type LayoutProps = { children: ReactElement; title: string };
 export function Layout({ children, title }: LayoutProps) {
   return (
     <div className="h-full min-h-screen relative">
-      <header className="w-full h-72 bg-black rounded-b-[6.25rem] pt-11 flex justify-center items-center text-center md:px-32">
+      <header className="w-full h-72 bg-black rounded-b-[6.25rem] pt-40 flex justify-center items-center text-center md:px-32">
         <img
           className="w-44 h-20 absolute left-[10%] top-10 cursor-pointer"
           width={256}
           height={102}
-          src="/Logo.png"
+          src="/Logo.svg"
           alt="Logomarca do site com o texto: 'Wesley News'"
         />
         <h1 className="w-8/12 text-white text-4xl">{title}</h1>
@@ -18,7 +18,7 @@ export function Layout({ children, title }: LayoutProps) {
 
       {children}
 
-      <footer>
+      <footer className="w-full h-28 absolute bottom-0 bg-black flex items-center justify-center">
         <small>&copy; Copyright 2022, Wesley News</small>
       </footer>
     </div>
