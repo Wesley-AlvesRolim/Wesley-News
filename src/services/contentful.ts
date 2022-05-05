7cb93a9eae4b61dfb46c546206063f318acee3b8
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-05-05 11:27:12 -0300

    chore: setup contentful client

---
+++ src/services/contentful.ts
@@ -0,0 +1,16 @@
+import * as contentful from "contentful";
+
+const client = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_DELIVERY_API_KEY,
+});
+
+export const getSeveralNews = async () => {
+  const { items } = await client.getEntries();
+  return items;
+};
+
+export const getSingleNews = async (newsId: string) => {
+  const singleNews = await client.getEntry(newsId);
+  return singleNews;
+};
