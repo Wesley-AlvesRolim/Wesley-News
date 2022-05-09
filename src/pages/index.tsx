1f4caef3a671c51cd8a843103ddc209de6c60e14
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-05-09 12:25:59 -0300

    feat: add get more news functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
-import { GetServerSideProps } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import { Layout } from "../components/Layout";
 import { HighlightNews } from "../components/HighlightNews";
 import { NewsCard } from "../components/NewsCard";
 import { getSeveralNews } from "../services/contentful";
 
-export default function Home({ severalNews: severalNewsByProps }) {
-  const [highlightNews, ...severalNews] = severalNewsByProps;
+export default function Home() {
+  const [newsTotal, setNewsTotal] = useState(0);
+  const [newsGroup, setNewsGroup] = useState([]);
+  const [highlightNews, ...severalNews] = newsGroup;
+
+  async function getNews(limit: number) {
+    const { origin } = window.location;
+    const response = await fetch(`${origin}/api/newsPagination/${limit}`);
+    const { items, total } = await response.json();
+    setNewsGroup(items);
+    setNewsTotal(total);
+  }
+  useEffect(() => {
+    getNews(5);
+  }, []);
   return (
     <>
       <Head>
@@ -19,13 +32,34 @@ export default function Home({ severalNews: severalNewsByProps }) {
       </Head>
 
       <main className="text-black dark:text-gray-50">
-        <HighlightNews news={highlightNews} />
+        {newsGroup.length > 0 ? (
+          <>
+            <HighlightNews news={highlightNews} />
 
-        <div className="max-w-[80%] m-auto mt-4 flex flex-wrap justify-center gap-16">
-          {severalNews.map((news) => (
-            <NewsCard key={news.sys.id} news={news} />
-          ))}
-        </div>
+            <div className="max-w-[80%] m-auto mt-4 flex flex-wrap justify-center gap-16">
+              {severalNews.map((news) => (
+                <NewsCard key={news.sys.id} news={news} />
+              ))}
+            </div>
+
+            {newsGroup.length >= newsTotal ? (
+              ""
+            ) : (
+              <div className="mt-8">
+                <button
+                  title="Botão responsável por carregar mais notícias"
+                  type="button"
+                  className="block w-full max-w-[10rem] m-auto p-2 bg-[#a8001c] text-white font-bold hover:brightness-125 transition-colors"
+                  onClick={() => getNews(newsGroup.length + 4)}
+                >
+                  Mais notícias
+                </button>
+              </div>
+            )}
+          </>
+        ) : (
+          "CARREGANDO"
+        )}
       </main>
     </>
   );
