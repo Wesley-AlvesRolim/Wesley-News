ed7308d366f2db78c9cd1b5d02ad7e0710c47b9b
Wesley Alves Rolim <walvesrolim@gmail.com>
2022-05-09 16:31:38 -0300

    feat: add last news to news page

--- src/pages/news/[id].tsx
+++ src/pages/news/[id].tsx
@@ -1,9 +1,10 @@
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { BLOCKS } from "@contentful/rich-text-types";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
 import { Layout } from "../../components/Layout/";
-import { getSingleNews } from "../../services/contentful";
+import { getSingleNews, getSeveralNews } from "../../services/contentful";
+import { NewsList } from "../../components/NewsList";
 
 const configsRender = {
   renderNode: {
@@ -18,68 +19,85 @@ const configsRender = {
   },
 };
 
-export default function News({ news }) {
+export default function News({ currentNews, latestNews }) {
   return (
     <>
       <Head>
-        <title>{news.fields.title}</title>
-        <meta name="description" content={news.fields.description} />
+        <title>{currentNews.fields.title}</title>
+        <meta name="description" content={currentNews.fields.description} />
         <link
           rel="canonical"
-          href={"https://wesley-news.vercel.app/news/" + news.sys.id}
+          href={"https://wesley-news.vercel.app/news/" + currentNews.sys.id}
         />
         <link rel="shortcut icon" href="/favicon.png" type="image/png" />
 
         <meta property="og:site_name" content="Wesley news" />
-        <meta property="og:title" content={news.fields.title} />
-        <meta property="og:description" content={news.fields.description} />
+        <meta property="og:title" content={currentNews.fields.title} />
+        <meta
+          property="og:description"
+          content={currentNews.fields.description}
+        />
         <meta
           property="og:url"
-          content={"https://wesley-news.vercel.app/news/" + news.sys.id}
+          content={"https://wesley-news.vercel.app/news/" + currentNews.sys.id}
         />
         <meta
           property="og:image"
-          content={"https:" + news.fields.thumbnail.fields.file.url}
+          content={"https:" + currentNews.fields.thumbnail.fields.file.url}
         />
         <meta property="og:locale" content="pt-br" />
         <meta property="og:type" content="website" />
       </Head>
-      <Layout title={news.fields.title}>
-        <main className="w-4/5 min-h-[21.5rem] py-4 m-auto sm:p-14">
-          <div className="w-full max-w-4xl m-auto">
-            <img
-              className="w-full rounded-[1.875rem] object-cover"
-              src={news.fields.thumbnail.fields.file.url}
-              alt={`Foto de capa da notícia com o título: ${news.fields.title}`}
-            />
-          </div>
-
-          <article className="text-black dark:text-white">
-            <section className="mt-5 whitespace-pre-wrap break-words news-content">
-              {documentToReactComponents(news.fields.content, configsRender)}
-            </section>
-
-            <footer className="mt-8 text-gray-700 dark:text-gray-400 flex hover:text-gray-800 dark:hover:text-gray-300 transition-colors">
+      <Layout title={currentNews.fields.title}>
+        <>
+          <main className="w-4/5 min-h-[21.5rem] py-4 m-auto sm:p-14">
+            <div className="w-full max-w-4xl m-auto">
               <img
-                className="w-10 h-10 mr-4 rounded-full"
-                src={
-                  news.fields.author.fields.image?.fields.file.url
-                    ? "https:" + news.fields.author.fields.image.fields.file.url
-                    : "/user-profile.png"
-                }
-                alt={`Foto do autor dessa notícia. Seu nome é: ${news.fields.author.fields.name}`}
+                className="w-full rounded-[1.875rem] object-cover"
+                src={currentNews.fields.thumbnail.fields.file.url}
+                alt={`Foto de capa da notícia com o título: ${currentNews.fields.title}`}
               />
-              <div className="flex flex-col">
-                <h3> {news.fields.author.fields.name}</h3>
-                <time>
-                  {new Intl.DateTimeFormat("pt-BR", {
-                    dateStyle: "full",
-                  }).format(new Date(news.sys.createdAt))}
-                </time>
-              </div>
-            </footer>
-          </article>
-        </main>
+            </div>
+
+            <article className="text-black dark:text-white">
+              <section className="mt-5 whitespace-pre-wrap break-words news-content">
+                {documentToReactComponents(
+                  currentNews.fields.content,
+                  configsRender
+                )}
+              </section>
+
+              <footer className="mt-8 text-gray-700 dark:text-gray-400 flex hover:text-gray-800 dark:hover:text-gray-300 transition-colors">
+                <img
+                  className="w-10 h-10 mr-4 rounded-full"
+                  src={
+                    currentNews.fields.author.fields.image?.fields.file.url
+                      ? "https:" +
+                        currentNews.fields.author.fields.image.fields.file.url
+                      : "/user-profile.png"
+                  }
+                  alt={`Foto do autor dessa notícia. Seu nome é: ${currentNews.fields.author.fields.name}`}
+                />
+                <div className="flex flex-col">
+                  <h3> {currentNews.fields.author.fields.name}</h3>
+                  <time>
+                    {new Intl.DateTimeFormat("pt-BR", {
+                      dateStyle: "full",
+                    }).format(new Date(currentNews.sys.createdAt))}
+                  </time>
+                </div>
+              </footer>
+            </article>
+          </main>
+          {latestNews.length > 0 && (
+            <div className="flex flex-col">
+              <h2 className="mt-12 font-bold text-3xl self-center">
+                Veja também
+              </h2>
+              <NewsList newsGroup={latestNews} />
+            </div>
+          )}
+        </>
       </Layout>
     </>
   );
@@ -95,10 +113,18 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params;
 
-  const news = await getSingleNews(id as string);
+  const currentNews = await getSingleNews(id as string);
+
+  const { items } = await getSeveralNews(3);
+  const latestNews = [];
+  items.forEach((news) => {
+    if (news.sys.id !== id) latestNews.push(news);
+  });
+  if (latestNews.length === 3) latestNews.pop();
   return {
     props: {
-      news,
+      currentNews,
+      latestNews,
     },
   };
 };
